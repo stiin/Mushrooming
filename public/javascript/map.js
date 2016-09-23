7121735ecd4ea0d13350dea8edb89e3b99b56bc1
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-09-23 06:26:50 +0200

    Large changes to these files. Updated according to the code of the main page.

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,85 +1,146 @@
-// CORS needs to enabled on the server! Now it is handled with this function
-(function() {
-  var cors_api_host = 'cors-anywhere.herokuapp.com';
-  var cors_api_url = 'https://' + cors_api_host + '/';
-  var slice = [].slice;
-  var origin = window.location.protocol + '//' + window.location.host;
-  var open = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function() {
-    var args = slice.call(arguments);
-    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-    if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-      targetOrigin[1] !== cors_api_host) {
-        args[1] = cors_api_url + args[1];
+var map;
+
+function initMap() {   
+
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup-content');
+    var closer = document.getElementById('popup-closer');
+
+    var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
+      element: container,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250
       }
-    return open.apply(this, args);
-  };
-})();
+    }));
+  
+    closer.onclick = function() {
+      overlay.setPosition(undefined); 
+      closer.blur();
+      return false
+    };
 
-
-function initMap() {
-	var map;
-
-	// WFS -> is needed for getting interaction with the features
-	var vectorSource = new ol.source.Vector({
-  	  format: new ol.format.GeoJSON(),
-  	  url: function(extent, resolution, projection) {
-        return "http://www.stiin.se:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:mushroom_findings&outputFormat=application%2Fjson";
-  	  },
-  	  strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
+  
+    // WFS source
+    var vectorSource = new ol.source.Vector({
+      format: new ol.format.GeoJSON(),
+      url: function(extent, resolution, projection) {
+        return "/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:mushroom_findings&outputFormat=application%2Fjson&srsname=EPSG:3857&" + 'CQL_FILTER=(bbox(the_geom,' + extent.join(',') + 
+	",'EPSG:3857'" + "))";
+      },
+      strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
         maxZoom: 16
-  	  }))
-	});
-	  
-    var geojson_test = new ol.layer.Vector({
-  	  source: vectorSource,
-  	  style: new ol.style.Style({
-	    image: new ol.style.Circle({
-	      radius: 6,
-	      fill: new ol.style.Fill({color: 'rgba(57,155,221,1)'}),
-	      stroke: new ol.style.Stroke({color: 'rgba(31,119,180,1)', width: 2})
-    	})
- 	  })
-	});
+      }))
+    });
 
-	// WMS
-	var mapbox_layer = new ol.layer.Tile({
+    // Making clusters out of the mushroom finding geojson points
+    var mushroom_cluster = new ol.source.Cluster({
+      source: vectorSource
+    });
+
+    // Making a layer of these clusters and assigning a styling
+    var mushroom_clusterLayer = new ol.layer.Vector({
+      source: mushroom_cluster,
+      style: new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 6,
+          fill: new ol.style.Fill({color: 'rgba(57,155,221,1)'}),
+          stroke: new ol.style.Stroke({color: 'rgba(31,119,180,1)', width: 2})
+        })
+      })
+    });
+
+
+    // WMS
+    var mapbox_layer = new ol.layer.Tile({
       source: new ol.source.XYZ({
         url: 'https://api.mapbox.com/styles/v1/stiin/cit7lsx18000y2vqn067wj5zy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3RpaW4iLCJhIjoiY2l0NnpsN2h5MDAwZjJ1bWZjOGg1d2ltOSJ9.SolRo3rDm25r9tXBTKrnoQ'
       })
+    }); 
+
+
+    // Setting view parameters
+    view = new ol.View({
+      center: ol.proj.transform([18.069248, 59.324783], 'EPSG:4326', 'EPSG:3857'),
+      zoom:     12,
+      minZoom:  5,
+      maxZoom:  20  // Mapbox does not allow to zoom in to far.
+    });
+    
+    // Declaring a map
+    map = new ol.Map({
+      //new ol.layer.Tile({source: new ol.source.OSM()}),
+      layers: [mapbox_layer, mushroom_clusterLayer],
+      overlays: [overlay],
+      target: 'map',
+      view: view
+    }); 
+    var sidebar = $('#sidebar').sidebar();    
+    
+    // Makes the layer mushroom_clusterLayer as selectable
+    selectInteraction = new ol.interaction.Select({
+        layers: function(layer) {
+            return layer.get('selectable') == true;
+        }//,
+          //style: [selectedstyle]
+    });
+    map.getInteractions().extend([selectInteraction]);
+
+    // Set "selectable" should only work so that you can click them and the features inside the clusters open -> zoomify in a sense
+    mushroom_clusterLayer.set('selectable', true); 
+
+    selectedFeatures = selectInteraction.getFeatures(); 
+    selectedFeatures.on('add', function(event) {
+      feature = event.target.item(0);
+      
+      geometry = feature.getGeometry();
+      coordinates = geometry.getCoordinates();
+
+      // inserts every name from the feature/features into popup
+      features = feature.get('features');
+      for (var i = 0; i < features.length; i++) {
+	    mushrooms = features[i].get('name');
+	    content.innerHTML += mushrooms + ", ";
+      }
+
+      overlay.setPosition(coordinates);
+    });
+
+
+
+
+
+    var geolocation, accuracyFeature, accuracyBuffer, coordinates;
+    var locateButton = document.getElementById('locate');
+
+    geolocation = new ol.Geolocation({
+      trackingOptions: {
+        enableHighAccuracy: true
+      },
+      projection: view.getProjection(),
+      tracking: true
     });	
 
-	// Setting view parameters
-	view = new ol.View({
-	  center: ol.proj.transform([18.069248, 59.324783], 'EPSG:4326', 'EPSG:3857'),
-	  zoom: 12
-	});
-	
-	// Declaring a map with the MapBox tilelayer and a WFS geojson layer
-	map = new ol.Map({
-  	  layers: [
-       mapbox_layer,
-	   geojson_test
-  	  ],
-  	  target: 'map',
-  	  view: view
-	});
-	
-	// Makes the layer geojson_test as selectable and post layer information into console
-	selectInteraction = new ol.interaction.Select({
-          layers: function(layer) {
-            return layer.get('selectable') == true;
-          }
-          //style: [selectedstyle]
-      	});
-	map.getInteractions().extend([selectInteraction]);
-	geojson_test.set('selectable',true); 
+    accuracyFeature = new ol.Feature();
+    accuracyBuffer = new ol.layer.Vector({
+      map: map,
+      source: new ol.source.Vector({
+        features: [accuracyFeature]
+      })
+    });
 
-	selectedFeatures = selectInteraction.getFeatures();	
-	selectedFeatures.on('add', function(event) {	
-	  feature = event.target.item(0);
-	  info = feature.get('name');
-	  console.log(info);
-	});
-	
+    map.addLayer(accuracyBuffer); 
+
+    locateButton.addEventListener('click',function(event) {
+      coordinates = geolocation.getPosition();
+      view.setZoom(14);
+      view.setCenter(coordinates);
+      accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+    });
+
+    geolocation.on('error', function(error) {
+      console.log(error);
+    });
+
+
 }

e7a68831219f0142b94b3e5e7a9bd99f3afd2c59
hitomi <hitomi@example.com>
2016-10-15 17:07:24 +0200

    geolocation cmp!

--- public/javascript/geolocation.js
+++ public/javascript/geolocation.js
@@ -12,7 +12,7 @@ function updatePositionMap(){
     if(acc != null) {
         accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
     }
-    
+
     if(!hasbeencentered) {
         view.setCenter(coordinate);
         hasbeencentered = true;
@@ -47,7 +47,6 @@ function autoLocationClicked(checkbox){
         console.log("Auto Location has been turned on");
 
         geolocation.on('error', positionUpdatingError);
-        drawRoute();
 
     }
     else {
@@ -65,20 +64,92 @@ function autoLocationClicked(checkbox){
     }
 }
 
-// pk.eyJ1Ijoic3RpaW4iLCJhIjoiY2l0NnpsN2h5MDAwZjJ1bWZjOGg1d2ltOSJ9.SolRo3rDm25r9tXBTKrnoQ
+///////////////////////////////////////////////////////////////////////////////////////////////
+// coord should be strings
+function navigateToMush(startLat, startLon, endLat, endLon, travelMode) {
+    var mode = [travelMode];// walking,driving,cycling + driving can integrate to pg routing
 
-function navigateToMush(lat,lon){
+    var points = startLat + "," + startLon + ";" + endLat + "," + endLon; //"17.813372,59.454118;17.798343,59.450776" //"startLon,startLat;endLon,endLat"
 
+// You need to edit the url for each mode /mapbox.@@@
+    var directionsUrl = 'http://api.tiles.mapbox.com/v4/directions/mapbox.' + travelMode + '/' +
+        points + '.json?access_token=' + accessToken;
+    $.get(directionsUrl, function (data) {
+        var route = data.routes[0].geometry.coordinates;
+        route = route.map(function (point) {
+            return [point[1], point[0]];
+        });
+
+        var multiP = new ol.geom.MultiPoint();
+        var transed = [];
+        for (var i = 0; i < route.length; i++) {
+
+            var pos = [route[i][1], route[i][0]];
+
+            pos = ol.proj.fromLonLat(pos);
+            transed.push(pos);
+            console.log('xy: ' + pos);
+            var geomPoint = new ol.geom.Point(pos);
+            multiP.appendPoint(geomPoint);
+        }
+
+        console.log("result was " + route);
+
+        return multiP;
+    });
 }
 
-// from example
+function drawMultiplePointsRoute(multiP){
 
+        var
+            vectorSource = new ol.source.Vector(),
+            vectorLayer = new ol.layer.Vector({
+                source: vectorSource
+            });
+
+        var routePath = new ol.format.Polyline({
+        }).readGeometry(multiP);
+        routePath.flatCoordinates = multiP.flatCoordinates;
+        var feature = new ol.Feature({
+            type: 'route',
+            geometry: routePath
+        });
+
+        var routeStyle = new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                width: 4, color: [255, 0, 0, 0.9]
+            })
+        })
+
+        feature.setStyle(routeStyle);
+        vectorSource.addFeature(feature);
+        map.addLayer(vectorLayer);
+        view.setCenter(pos);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// You need to add this to the index.html:
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/ol3/3.18.2/ol-debug.js"></script>
+//<script src="https://api.tiles.mapbox.com/mapbox.js/v2.1.5/mapbox.js"></script>
+//<link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.5/mapbox.css' rel='stylesheet' />
+
+// ↓ Key: pk.eyJ1Ijoic3RpaW4iLCJhIjoiY2l0NnpsN2h5MDAwZjJ1bWZjOGg1d2ltOSJ9.SolRo3rDm25r9tXBTKrnoQ ↓
 accessToken = 'pk.eyJ1Ijoic3RpaW4iLCJhIjoiY2l0NnpsN2h5MDAwZjJ1bWZjOGg1d2ltOSJ9.SolRo3rDm25r9tXBTKrnoQ';
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 function drawRoute() {
     console.log("start drawRoute");
-    var points = "17.813372,59.454118;17.798343,59.450776"
-    var directionsUrl = 'http://api.tiles.mapbox.com/v4/directions/mapbox.driving/' +
+    // Location Initial Setting //
+    var startLat;
+    var startLon;
+    var endLat;
+    var endLon;
+    var mode = [];// walking,driving,cycling + driving can integrate to pg routing
+
+    var points = "17.813372,59.454118;17.798343,59.450776" //"startLon,startLat;endLon,endLat"
+
+// You need to edit the url for each mode /mapbox.@@@
+    var directionsUrl = 'http://api.tiles.mapbox.com/v4/directions/mapbox.walking/' +
         points + '.json?access_token=' + accessToken;
     $.get(directionsUrl, function(data) {
         var route = data.routes[0].geometry.coordinates;
@@ -127,3 +198,4 @@ function drawRoute() {
         view.setCenter(pos);
     });
 }
+
